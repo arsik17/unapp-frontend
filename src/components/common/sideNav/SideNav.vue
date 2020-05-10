d09f089abbd7ca4368f8e3a3fbab6b5f455475f0
Crepiks <sayazhan.onlassyn@mail.ru>
2020-05-10 23:46:30 +0600

    refactor: move side bar into separate component

---
+++ src/components/common/sideNav/SideNav.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav class="nav">
+    <div class="logo"></div>
+    <div class="nav__links">
+      <router-link to="#" class="nav__link">
+        <a-icon type="home" theme="filled" class="nav__icon" />
+        <span class="nav__link-text">Home</span>
+      </router-link>
+      <router-link to="#" class="nav__link">
+        <a-icon type="bank" theme="filled" class="nav__icon" />
+        <span class="nav__link-text">Universities</span>
+      </router-link>
+      <router-link to="#" class="nav__link">
+        <a-icon type="idcard" theme="filled" class="nav__icon" />
+        <span class="nav__link-text">Profile</span>
+      </router-link>
+      <router-link to="#" class="nav__link">
+        <a-icon type="copy" theme="filled" class="nav__icon" />
+        <span class="nav__link-text">Documents</span>
+      </router-link>
+      <router-link to="#" class="nav__link">
+        <a-icon type="trophy" theme="filled" class="nav__icon" />
+        <span class="nav__link-text">Progress</span>
+      </router-link>
+    </div>
+    <footer class="nav__footer">
+      <a-icon type="api" theme="filled" class="nav__footer-icon" />
+      <span class="nav__footer-text">Exit</span>
+    </footer>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.nav {
+  padding: 5vh 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.nav__link,
+.nav__footer {
+  padding: 15px 20px;
+  display: flex;
+  align-items: center;
+  font-size: 17px;
+  color: #ffffff;
+  border-radius: 10px;
+  box-sizing: border-box;
+  transition: 300ms;
+}
+
+.nav__link:hover,
+.nav__footer:hover {
+  background-color: #1574cc;
+}
+
+.nav__icon,
+.nav__footer-icon {
+  margin-right: 20px;
+}
+</style>
--- src/views/layouts/DefaultLayout.vue
+++ src/views/layouts/DefaultLayout.vue
@@ -1,35 +1,7 @@
 <template>
   <div class="default-layout">
     <div class="defaut-layout__nav-wrapper">
-      <nav class="default-layout__nav nav">
-        <div class="logo"></div>
-        <div class="nav__links">
-          <router-link to="#" class="nav__link">
-            <a-icon type="home" theme="filled" class="nav__icon" />
-            <span class="nav__link-text">Home</span>
-          </router-link>
-          <router-link to="#" class="nav__link">
-            <a-icon type="bank" theme="filled" class="nav__icon" />
-            <span class="nav__link-text">Universities</span>
-          </router-link>
-          <router-link to="#" class="nav__link">
-            <a-icon type="idcard" theme="filled" class="nav__icon" />
-            <span class="nav__link-text">Profile</span>
-          </router-link>
-          <router-link to="#" class="nav__link">
-            <a-icon type="copy" theme="filled" class="nav__icon" />
-            <span class="nav__link-text">Documents</span>
-          </router-link>
-          <router-link to="#" class="nav__link">
-            <a-icon type="trophy" theme="filled" class="nav__icon" />
-            <span class="nav__link-text">Progress</span>
-          </router-link>
-        </div>
-        <footer class="nav__footer">
-          <a-icon type="api" theme="filled" class="nav__footer-icon" />
-          <span class="nav__footer-text">Exit</span>
-        </footer>
-      </nav>
+      <side-nav class="default-layout__nav" />
     </div>
     <div class="default-layout__content">
       <router-view></router-view>
@@ -38,7 +10,13 @@
 </template>
 
 <script>
-export default {};
+import SideNav from "@/components/common/sideNav/SideNav";
+
+export default {
+  components: {
+    "side-nav": SideNav
+  }
+};
 </script>
 
 <style scoped>
@@ -67,35 +45,5 @@ export default {};
   width: 80%;
   box-sizing: border-box;
   border: 1px solid green;
-}
-
-.nav {
-  padding: 5vh 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.nav__link,
-.nav__footer {
-  padding: 15px 20px;
-  display: flex;
-  align-items: center;
-  font-size: 17px;
-  color: #ffffff;
-  border-radius: 10px;
-  box-sizing: border-box;
-  transition: 300ms;
-}
-
-.nav__link:hover,
-.nav__footer:hover {
-  background-color: #1574cc;
-}
-
-.nav__icon,
-.nav__footer-icon {
-  margin-right: 20px;
 }
 </style>
