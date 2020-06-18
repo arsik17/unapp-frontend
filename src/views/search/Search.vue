f3d71a32d4561bc9b5d4e341bc28ed6838b46aec
Crepiks <sayazhan.onlassyn@mail.ru>
2020-06-18 23:27:36 +0600

    feat: create animation when user search

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -1,38 +1,53 @@
 <template>
   <div class="search">
-    <img
-      class="search__logo"
-      src="@/assets/images/logo_dark.svg"
-      alt="UnApp Logo"
-    />
-    <a-input-search
-      class="search__field"
-      size="large"
-      style="width: 50%"
-      placeholder="Find University"
-      enter-button="Search"
-      @search="onSearch"
-    />
+    <div
+      class="search__field-container"
+      :class="{ 'search__field-container_small': isSearching }"
+    >
+      <img
+        class="search__logo"
+        src="@/assets/images/logo_dark.svg"
+        alt="UnApp Logo"
+      />
+      <a-input-search
+        class="search__field"
+        size="large"
+        style="width: 50%"
+        placeholder="Find University"
+        enter-button="Search"
+        @search="handleSearch"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isSearching: false
+    };
+  },
   methods: {
-    onSearch() {
-      console.log("Searching");
+    handleSearch() {
+      this.isSearching = true;
     }
   }
 };
 </script>
 
 <style>
-.search {
+.search__field-container {
+  height: 60vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 60vh;
+  transition: 1s ease;
+}
+
+.search__field-container.search__field-container_small {
+  height: 15vh;
 }
 
 .search__logo {
