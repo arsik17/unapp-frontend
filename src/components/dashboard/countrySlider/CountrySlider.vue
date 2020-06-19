51290ee0265815699a56178fa1432f37da244dbe
Crepiks <sayazhan.onlassyn@mail.ru>
2020-06-19 16:27:26 +0600

    feat: style carousel on the Dashboard view

--- src/components/dashboard/countrySlider/CountrySlider.vue
+++ src/components/dashboard/countrySlider/CountrySlider.vue
@@ -1,11 +1,23 @@
 <template>
   <hooper class="country-slider" :itemsToShow="3">
-    <slide class="country-slider__slide">slide 1</slide>
-    <slide class="country-slider__slide">slide 2</slide>
-    <slide class="country-slider__slide">slide 3</slide>
-    <slide class="country-slider__slide">slide 4</slide>
-    <slide class="country-slider__slide">slide 5</slide>
-    <slide class="country-slider__slide">slide 6</slide>
+    <slide class="country-slider__slide-container">
+      <div class="country-slider__slide">Slide 1</div>
+    </slide>
+    <slide class="country-slider__slide-container">
+      <div class="country-slider__slide">Slide 2</div>
+    </slide>
+    <slide class="country-slider__slide-container">
+      <div class="country-slider__slide">Slide 3</div>
+    </slide>
+    <slide class="country-slider__slide-container">
+      <div class="country-slider__slide">Slide 4</div>
+    </slide>
+    <slide class="country-slider__slide-container">
+      <div class="country-slider__slide">Slide 5</div>
+    </slide>
+    <slide class="country-slider__slide-container">
+      <div class="country-slider__slide">Slide 6</div>
+    </slide>
   </hooper>
 </template>
 
@@ -24,11 +36,24 @@ export default {
 <style scoped>
 .country-slider {
   width: 100%;
+  outline: none;
 }
 
-.country-slider__slide {
+.country-slider__slide-container {
+  padding: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.country-slider__slide {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  background-color: #3e2179;
+  border-radius: 10px;
 }
 </style>
