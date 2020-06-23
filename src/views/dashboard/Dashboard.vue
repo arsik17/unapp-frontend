1e1a4df689f7ad3223e92743279c502cb53a689e
oryncool <aba2003aba@gmail.com>
2020-06-23 21:05:35 +0600

    fix: change file names

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,23 +1,9 @@
 <template>
   <div class="dashboard">
-    <div class="card">
-      <div class="card__text">
-        <h2 class="card__heading">Find University of your dream</h2>
-        <p class="card__paragraph">
-          We provide a full-featured platform for management your university
-          application process. You can find the best university for yourself in
-          one click and save it in account. Let's rocket your future career!
-        </p>
-        <a-button @click="$router.push('/search')" type="primary" size="large"
-          >Find something awesome</a-button
-        >
-      </div>
-      <img src="@/assets/images/swinging_doodle.svg" alt="Swinging doodle" />
-    </div>
+    <h1 class="dashboard__heading">STUDY WHEREVER YOU WANT</h1>
+    <country-grid />
     <h2 class="dashboard__heading">Choose country</h2>
     <country-slider class="dashboard__country-slider" />
-    <h1 class="dashboard__head">STUDY WHEREVER YOU WANT</h1>
-    <country-grid />
   </div>
 </template>
 
@@ -40,34 +26,7 @@ export default {
   padding-bottom: 50px;
 }
 
-.card {
-  padding: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  box-sizing: border-box;
-}
-
-.card__text {
-  width: 70%;
-}
-
-.card__heading {
-  font-size: 30px;
-}
-
-.card__paragraph {
-  margin-bottom: 20px;
-}
-
 .dashboard__heading {
-  margin-top: 50px;
-  font-size: 26px;
-}
-
-.dashboard__head {
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   font-weight: 400;
   text-align: center;
